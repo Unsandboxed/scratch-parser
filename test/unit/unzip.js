061a5b9d6c4dbd454f5458779d23f301b4aa48d1
Muffin <muffin@muffin.ink>
2024-05-19 19:22:56 -0500

    Replace jszip with @turbowarp/jszip
    It has some optimizations that slightly improve unzipping time

--- test/unit/unzip.js
+++ test/unit/unzip.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var test = require('tap').test;
-var JSZip = require('jszip');
+var JSZip = require('@turbowarp/jszip');
 var unzip = require('../../lib/unzip');
 
 var fixtures = {
